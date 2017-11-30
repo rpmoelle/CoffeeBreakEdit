d0da8a449a3ecaa30424779d6a0a0da34fcf123e
Rachel Moeller <parrishwork@gmail.com>
2017-11-30 15:29:31 -0500

    Cup Follows but is Uggo

--- Assets/Scripts/RedCup.cs
+++ Assets/Scripts/RedCup.cs
@@ -9,11 +9,14 @@ public class RedCup : MonoBehaviour {
     public bool working;
     public GameObject itemNeeded; //item needed inorder to interact with coffee machine
     public Animator anim;
+    public SpriteRenderer cupOnPlayer;
 
     public void DoInteraction()
     { 
         //pick up and put in inventory
         gameObject.SetActive(false);
+        //turn on the version she is holding
+        cupOnPlayer.enabled = true;
     }
 
     public void pourCoffee()
