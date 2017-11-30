a52732291a307971645958842f96494468264d85
Rachel Moeller <parrishwork@gmail.com>
2017-11-30 16:13:14 -0500

    I added a shadow to the render order I AM THE DEVIL

--- Assets/Scripts/SpriteDepth.cs
+++ Assets/Scripts/SpriteDepth.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpriteDepth : MonoBehaviour {
     SpriteRenderer ren;
+    public SpriteRenderer shadow;
 	// Use this for initialization
 	void Start () {
         ren = GetComponent<SpriteRenderer>();
@@ -14,6 +15,7 @@ public class SpriteDepth : MonoBehaviour {
         //change the sprite depth based on it height on the screen
 
         ren.sortingOrder = (int)(transform.position.y*-100f);
+        shadow.sortingOrder = ren.sortingOrder - 200;
 
 	}
 }
